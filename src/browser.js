755797c47c03326ec194cc532b419c9e85f92b33
Alexander Seferinkyn <Alexander Seferinkyn>
2016-03-03 01:36:51 +0200

    initial

---
+++ src/browser.js
@@ -0,0 +1,7 @@
+/* browser.js - Browser bootstrap
+ *
+ * Copyright (C) 2016 Alexander Seferinkyn
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
